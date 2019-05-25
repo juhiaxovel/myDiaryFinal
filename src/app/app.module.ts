f79d12ddd94272e6966573e2426f03469c55074e
juhiaxovel <juhi.axovel@gmail.com>
2019-05-26 04:41:25 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DiaryEntryComponent } from './diary-entry/diary-entry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DiaryEntryComponent
   ],
   imports: [
     BrowserModule,
